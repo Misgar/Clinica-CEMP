8d28af51b578de3cd7884dcf6d623898a86c51a0
saobernardo <bernardo.pinheiro@hotmail.com>
2019-04-23 20:28:43 -0300

    Added all the necessary models

---
+++ application/models/ModelConsulta.php
@@ -0,0 +1,9 @@
+<?php
+
+  defined('BASEPATH') OR exit('No direct script access allowed');
+
+  class ModelConsulta extends CI_Model{
+
+  }
+
+  ?>
---
+++ application/models/Model_pgto.php
@@ -0,0 +1,9 @@
+<?php
+
+  defined('BASEPATH') OR exit('No direct script access allowed');
+
+  class ModelPgto extends CI_Model{
+
+  }
+
+  ?>
