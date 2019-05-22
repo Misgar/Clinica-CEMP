7bc43f266d68bff8e3b802d5e1559ac54b9221de
saobernardo <bernardo.pinheiro@hotmail.com>
2019-05-22 19:23:28 -0300

    Added some css scrits and fixed the base_url code to reference the css folder

--- application/views/index.php
+++ application/views/index.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="pt" dir="pt-br">
   <head>
-    <link rel="stylesheet" type="text/css" href="<?php base url('css/') ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/estilo.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/normalize.css') ?>">
     <title></title>
   </head>
   <body>
--- sql/testes_query.sql
+++ sql/testes_query.sql
@@ -2,6 +2,6 @@ use clinica;
 
 select * from pessoa;
 
-delete from pessoa where id=1;
+delete from pessoa where id = 1;
 
 select * from deactivated_pessoa;
