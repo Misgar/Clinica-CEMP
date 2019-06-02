028424bb044ea182dcd334756795fe14185ed06a
Renato AO <renato.oliveira34@fatec.sp.gov.br>
2019-06-02 10:53:22 -0300

    Criado arquivo a parte para navbar, que será sempre solicitado com require caso necessario

--- application/views/index.php
+++ application/views/index.php
@@ -8,37 +8,7 @@
     <title></title>
   </head>
   <body>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="<? base_url('index.php')?>">CEMP</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(página atual)</span></a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Consultas
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Marcar Nova</a>
-              <a class="dropdown-item" href="#">Verificar</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Histórico</a>
-            </div>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Nome Paciente" aria-label="Pesquisar">
-          <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Pesquisar Paciente</button>
-        </form>
-      </div>
-    </nav>
-
+    <?php require_once('navbar.php')  ?>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
